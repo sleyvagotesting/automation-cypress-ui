37f2205ee2e48a186060f91c28535ef7ed1c3c53
Saul Leyva Gonzalez <saul.leyva@agilethought.com>
2025-06-24 09:42:50 -0600

    test cypress

---
+++ Test1.js
@@ -0,0 +1,8 @@
+//cypress-spec
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+
+      
+    })
+  })
